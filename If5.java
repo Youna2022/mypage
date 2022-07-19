c2d7fdabc349531bed1a00a02bb187a7f8cf7b66
Youna2022 <108061839+Youna2022@users.noreply.github.com>
2022-07-20 08:57:31 +0900

    Update If5.java

--- If5.java
+++ If5.java
@@ -1,6 +1,6 @@
 package ch03;
 
-public class If5 {
+public class If4 {
 	public static void main(String[] args) {
 		int score = Integer.parseInt(args[0]);
 		String grade = "";
@@ -30,40 +30,4 @@ public class If5 {
 		
 }
 	
-}
-
-package ch03;
-
-public class If4 {
-	public static void main(String[] args) {
-		int score = Integer.parseInt(args[0]);
-		String grade = "";
-
-		if (score >= 90) {
-			grade = "A";
-			if (score > 97) {
-				grade += "+";
-			} else if (score < 94) {
-				grade += "-";
-			}
-		} else if (80 <= score && score < 90) {
-			grade = "B";
-			if (score > 87) {
-				grade += "+";
-			} else if (score < 84) {
-				grade += "-";
-			}
-
-		} else if (70 < score) {
-			grade = "C";
-			if (score > 77) {
-				grade += "+";
-			} else if (score < 74) {
-				grade += "-";
-			}
-		} else {
-			grade = "F";}
-
-		System.out.println("당신점수는" + score + " 이고" + "학점은 " + grade + "입니다");
-	}
 }
